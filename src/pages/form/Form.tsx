4df579b48dfe241d45c0eb89d7a80afccf05b308
ismoildevuz <ismoildevuz@gmail.com>
2025-05-07 19:14:59 +0500

    feat: login & form pages

--- src/pages/form/Form.tsx
+++ src/pages/form/Form.tsx
@@ -1,3 +1,193 @@
+import {
+  Button,
+  Card,
+  DatePicker,
+  Flex,
+  Form,
+  Input,
+  Select,
+  Upload,
+} from "antd"
+import { PlusOutlined } from "@ant-design/icons"
+import "./style.scss"
+
 export default () => {
-  return <div>Form</div>
+  const [form] = Form.useForm()
+
+  return (
+    <div className="form-container">
+      <Form layout="vertical" form={form}>
+        <Flex vertical gap={24}>
+          <Card className="form-card">
+            <div className="form-group">
+              <Form.Item
+                label="Korxona"
+                name="company"
+                rules={[{ required: true }]}
+              >
+                <Select
+                  placeholder="Tanlang"
+                  showSearch
+                  optionFilterProp="label"
+                  options={[
+                    {
+                      value: "1",
+                      label: "164-son Qo‘qon temir yo‘l mashinalar bekati",
+                    },
+                  ]}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Xodim"
+                name="worker"
+                rules={[{ required: true }]}
+              >
+                <Select
+                  placeholder="Tanlang"
+                  showSearch
+                  optionFilterProp="label"
+                  options={[
+                    {
+                      value: "1",
+                      label: "4301-tiklash poyezdi",
+                    },
+                  ]}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Stansiya"
+                name="station"
+                rules={[{ required: true }]}
+              >
+                <Select
+                  placeholder="Tanlang"
+                  showSearch
+                  optionFilterProp="label"
+                  options={[
+                    {
+                      value: "1",
+                      label: "Abadan",
+                    },
+                  ]}
+                />
+              </Form.Item>
+            </div>
+          </Card>
+
+          <Card className="form-card">
+            <div className="form-group">
+              <Form.Item label="Kompyuter turi" name="type">
+                <Select
+                  placeholder="Tanlang"
+                  showSearch
+                  optionFilterProp="label"
+                  options={[
+                    {
+                      value: "1",
+                      label: "Noutbuk",
+                    },
+                    {
+                      value: "2",
+                      label: "Shaxsiy",
+                    },
+                  ]}
+                />
+              </Form.Item>
+              <Form.Item label="UPS" name="ups">
+                <Select
+                  placeholder="Tanlang"
+                  showSearch
+                  optionFilterProp="label"
+                  options={[
+                    {
+                      value: "1",
+                      label: "UPS Yo'q",
+                    },
+                    {
+                      value: "2",
+                      label: "UPS Bor",
+                    },
+                  ]}
+                />
+              </Form.Item>
+              <Form.Item label="Monitor" name="monitor">
+                <Select
+                  placeholder="Tanlang"
+                  showSearch
+                  optionFilterProp="label"
+                  options={[
+                    {
+                      value: "1",
+                      label: "FHD+",
+                    },
+                    {
+                      value: "2",
+                      label: "HD",
+                    },
+                  ]}
+                />
+              </Form.Item>
+              <Form.Item label="Printer" name="printer">
+                <Select
+                  placeholder="Tanlang"
+                  showSearch
+                  optionFilterProp="label"
+                  options={[
+                    {
+                      value: "1",
+                      label: "Printer Yo'q",
+                    },
+                    {
+                      value: "2",
+                      label: "HP LaserJet",
+                    },
+                  ]}
+                />
+              </Form.Item>
+            </div>
+          </Card>
+
+          <Card className="form-card">
+            <div className="form-group">
+              <Form.Item label="AnyDesk manzili" name="anydesk">
+                <Input placeholder="Kiriting" />
+              </Form.Item>
+              <Form.Item label="RAdmin manzili" name="radmin">
+                <Input placeholder="Kiriting" />
+              </Form.Item>
+              <Form.Item label="Inventar raqami" name="inventory">
+                <Input placeholder="Kiriting" />
+              </Form.Item>
+              <Form.Item label="Kirim bo‘lgan vaqt" name="receivedDate">
+                <DatePicker format="DD-MM-YYYY" className="full-width" />
+              </Form.Item>
+            </div>
+          </Card>
+
+          <Card className="form-card">
+            <div className="form-group">
+              <Form.Item
+                label="Kompyuter rasmi"
+                name="image"
+                valuePropName="fileList"
+                getValueFromEvent={(e) =>
+                  Array.isArray(e) ? e : e && e.fileList
+                }
+              >
+                <Upload beforeUpload={() => false} listType="picture-card">
+                  <PlusOutlined />
+                </Upload>
+              </Form.Item>
+
+              <Flex justify="end" align="end">
+                <Button type="primary" htmlType="submit">
+                  Saqlash
+                </Button>
+              </Flex>
+            </div>
+          </Card>
+        </Flex>
+      </Form>
+    </div>
+  )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,64 @@
-export default () => {
-  return <div>Home</div>
+import { useState } from "react"
+import { Button, Card, Form, Input, Typography } from "antd"
+import { LockOutlined, UserOutlined } from "@ant-design/icons"
+import "./style.scss"
+
+const LoginPage = () => {
+  const [loading, setLoading] = useState(false)
+
+  const onFinish = () => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }
+
+  return (
+    <div className="login-container">
+      <Card className="login-card">
+        <div className="login-logo">🌐</div>
+
+        <Typography.Title level={3} className="login-title">
+          Sign in
+        </Typography.Title>
+
+        <Typography.Paragraph type="secondary" className="login-subtitle">
+          Access the Service Management System to monitor and schedule
+          organizational tasks.
+        </Typography.Paragraph>
+
+        <Form
+          name="login"
+          size="large"
+          layout="vertical"
+          autoComplete="off"
+          onFinish={onFinish}
+        >
+          <Form.Item name="username" rules={[{ required: true }]}>
+            <Input prefix={<UserOutlined />} placeholder="Username" />
+          </Form.Item>
+
+          <Form.Item name="password" rules={[{ required: true }]}>
+            <Input.Password
+              placeholder="Password"
+              autoComplete="new-password"
+              prefix={<LockOutlined />}
+            />
+          </Form.Item>
+
+          <Button
+            block
+            type="primary"
+            htmlType="submit"
+            className="login-button"
+            loading={loading}
+          >
+            Log in
+          </Button>
+        </Form>
+      </Card>
+    </div>
+  )
 }
+
+export default LoginPage
